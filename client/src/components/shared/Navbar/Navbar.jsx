10be1a5bb33bb13674bae073d7e89d56f29b9f34
Farha Islam <suhifarhaislam@gmail.com>
2023-09-25 23:54:46 +0600

    adding navbar

--- client/src/components/shared/Navbar/Navbar.jsx
+++ client/src/components/shared/Navbar/Navbar.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import Container from '../Container/Container';
+import Logo from './Logo';
+import Search from './Search';
+import MenuDropdown from './MenuDropdown';
 
 const Navbar = () => {
     return (
-        <div className='fixed'>
-             
+        <div className='fixed w-full bg-white shadow-sm'>
+             <div className='py-4 border-b-[1px]'>
+                <Container>
+                    <div className='flex item-center justify-between gap-3 md:gap-0'>
+                        <Logo></Logo>
+                        <Search></Search>
+                        <MenuDropdown></MenuDropdown>
+                    </div>
+                </Container>
+             </div>
         </div>
     );
 };
